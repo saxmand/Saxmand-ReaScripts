735834fb2b2e4c9f7b3ff8d39b115b30770784eb
saxmand <saxmand@gmail.com>
2025-04-18 17:31:13 -0600

    new order test2

--- FX/Saxmand_FX Modulator Linking.lua
+++ FX/Saxmand_FX Modulator Linking.lua
@@ -2,8 +2,8 @@
 -- @author saxmand
 -- @version 0.0.1
 -- @provides
---    [nomain] Helpers/*.lua
---    [effect] Effects/Saxmand JSFX/FX Modulator Linking/4-in-1-out Modulator.jsfx
+--   [nomain] Helpers/*.lua
+--   [effect] Saxmand JSFX/FX Modulator Linking/*.jsfx
 
 -- @changelog
 --    + alpha release
